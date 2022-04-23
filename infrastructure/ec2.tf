fcf2295e6a542436243f3750b830de3c409ee579
Wellington Tobias <wellington_mt@yahoo.com>
2022-04-23 20:38:19 -0300

    Variáveis "literal"

--- infrastructure/ec2.tf
+++ infrastructure/ec2.tf
@@ -22,49 +22,51 @@ resource "aws_instance" "airflow" {
   instance_type               = "t3.medium"
   key_name                    = var.key_pair_name
   associate_public_ip_address = true
-  security_groups             = [aws_security_group.airflow_sg.id]
-  subnet_id                   = var.airflow_subnet_id
+  security_groups             = "airflow_sg"
+#  security_groups             = [aws_security_group.airflow_sg.id]
+  subnet_id                   = "subnet-08c574adfdb9b16ec"
+#  subnet_id                   = var.airflow_subnet_id
 }
 
 # Security group to allow acces to instance
-resource "aws_security_group" "airflow_sg" {
-  name        = "airflow_sg"
-  description = "Allow traffic on port 8080 for airflow"
-  vpc_id      = var.vpc_id
+# resource "aws_security_group" "airflow_sg" {
+#   name        = "airflow_sg"
+#   description = "Allow traffic on port 8080 for airflow"
+#   vpc_id      = var.vpc_id
 
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 8080
-    to_port          = 8080
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+#   ingress {
+#     description      = "TLS from VPC"
+#     from_port        = 8080
+#     to_port          = 8080
+#     protocol         = "tcp"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
 
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 5555
-    to_port          = 5555
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+#   ingress {
+#     description      = "TLS from VPC"
+#     from_port        = 5555
+#     to_port          = 5555
+#     protocol         = "tcp"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
 
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+#   ingress {
+#     description      = "TLS from VPC"
+#     from_port        = 22
+#     to_port          = 22
+#     protocol         = "tcp"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+#   egress {
+#     from_port        = 0
+#     to_port          = 0
+#     protocol         = "-1"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
 
-}
+# }
