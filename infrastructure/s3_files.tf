9647dcae8ec1cba8f162df1388223252ceefcd82
Wellington Tobias <wellington_mt@yahoo.com>
2022-04-22 22:04:52 -0300

    Criação diretório etl

--- infrastructure/s3_files.tf
+++ infrastructure/s3_files.tf
@@ -8,14 +8,14 @@ resource "aws_s3_bucket_object" "job_spark" {
 
 resource "aws_s3_bucket_object" "insere_postgres" {
   bucket = aws_s3_bucket.bucket-pa.id
-  key    = "emr-code/pyspark/insert_postgres.py"
-  source = "../insert_postgres.py"
-  etag   = filemd5("../insert_postgres.py")
+  key    = "etl/insert_postgres.py"
+  source = "../etl/insert_postgres.py"
+  etag   = filemd5("../etl/insert_postgres.py")
 }
 
 resource "aws_s3_bucket_object" "insere_elasticsearch" {
   bucket = aws_s3_bucket.bucket-pa.id
-  key    = "emr-code/pyspark/insert_elasticsearch.py"
-  source = "../insert_elasticsearch.py"
-  etag   = filemd5("../insert_elasticsearch.py")
+  key    = "etl/insert_elasticsearch.py"
+  source = "../etl/insert_elasticsearch.py"
+  etag   = filemd5("../etl/insert_elasticsearch.py")
 }
