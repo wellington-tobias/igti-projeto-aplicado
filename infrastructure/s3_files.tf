947e45c44f48c79161364a1386aed4bcda7b97a5
Wellington Tobias <wellington_mt@yahoo.com>
2022-04-22 21:54:09 -0300

    Alteração local dos scripts de insert

--- infrastructure/s3_files.tf
+++ infrastructure/s3_files.tf
@@ -9,13 +9,13 @@ resource "aws_s3_bucket_object" "job_spark" {
 resource "aws_s3_bucket_object" "insere_postgres" {
   bucket = aws_s3_bucket.bucket-pa.id
   key    = "etl/insert_postgres.py"
-  source = "../airflow/dags/include/insert_postgres.py"
-  etag   = filemd5("../airflow/dags/include/insert_postgres.py")
+  source = "../insert_postgres.py"
+  etag   = filemd5("../insert_postgres.py")
 }
 
 resource "aws_s3_bucket_object" "insere_elasticsearch" {
   bucket = aws_s3_bucket.bucket-pa.id
   key    = "etl/insert_elasticsearch.py"
-  source = "../airflow/dags/include/insert_elasticsearch.py"
-  etag   = filemd5("../airflow/dags/include/insert_elasticsearch.py")
+  source = "../insert_elasticsearch.py"
+  etag   = filemd5("../insert_elasticsearch.py")
 }
