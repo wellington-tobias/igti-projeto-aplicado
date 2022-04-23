bf4739a8b1c0b480d9fe0eeb5eaa87c5985b97a5
Wellington Tobias <wellington_mt@yahoo.com>
2022-04-22 21:49:46 -0300

    Ajustes Infra Airflow

--- airflow/install_dependencies.sh
+++ airflow/install_dependencies.sh
@@ -1,8 +1,5 @@
-# Postgres
 sudo apt-get install libpq-dev python-dev
 pip3 install psycopg2
-
-# Elasticsearch
 pip install boto3
 pip install opensearch-py
 pip install requests
--- libraries.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo python3 -m pip install \
-                        boto3 \
-	                    boto \
-		                requests \
-                        psycopg2-binary
