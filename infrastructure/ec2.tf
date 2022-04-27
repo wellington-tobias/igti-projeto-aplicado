f65895687b67c7ed238ccb0a63305946d260d8ca
Wellington Tobias <wellington_mt@yahoo.com>
2022-04-27 20:56:27 -0300

    security group airflow

--- infrastructure/ec2.tf
+++ infrastructure/ec2.tf
@@ -26,45 +26,45 @@
 #   subnet_id                   = var.airflow_subnet_id
 # }
 
-# # Security group to allow acces to instance
-# resource "aws_security_group" "airflow_sg" {
-#   name        = "airflow_sg"
-#   description = "Allow traffic on port 8080 for airflow"
-#   vpc_id      = var.vpc_id
+# Security group to allow acces to instance
+resource "aws_security_group" "airflow_sg" {
+  name        = "airflow_sg"
+  description = "Allow traffic on port 8080 for airflow"
+  vpc_id      = var.vpc_id
 
-#   ingress {
-#     description      = "TLS from VPC"
-#     from_port        = 8080
-#     to_port          = 8080
-#     protocol         = "tcp"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#     ipv6_cidr_blocks = ["::/0"]
-#   }
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
-#   ingress {
-#     description      = "TLS from VPC"
-#     from_port        = 5555
-#     to_port          = 5555
-#     protocol         = "tcp"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#     ipv6_cidr_blocks = ["::/0"]
-#   }
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 5555
+    to_port          = 5555
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
-#   ingress {
-#     description      = "TLS from VPC"
-#     from_port        = 22
-#     to_port          = 22
-#     protocol         = "tcp"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#     ipv6_cidr_blocks = ["::/0"]
-#   }
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
-#   egress {
-#     from_port        = 0
-#     to_port          = 0
-#     protocol         = "-1"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#     ipv6_cidr_blocks = ["::/0"]
-#   }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
-# }
+}
